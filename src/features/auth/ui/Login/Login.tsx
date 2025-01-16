43243e1a3fa4b2092c2a0f70f51ba6a634bd2619
EkaterinaZamkovoy <ekaterinazamkovoy@yandex.ru>
2025-01-16 14:29:36 +0300

    L16-react-hook-form

--- src/features/auth/ui/Login/Login.tsx
+++ src/features/auth/ui/Login/Login.tsx
@@ -1,41 +1,78 @@
 import { Button, Container } from 'common/components';
+import { SubmitHandler, useForm } from 'react-hook-form';
+
+type Inputs = {
+  email: string;
+  password: string;
+  rememberMe: boolean;
+};
 
 export const Login = () => {
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm<Inputs>({
+    defaultValues: { email: '', password: '', rememberMe: false },
+  });
+
+  const onSubmit: SubmitHandler<Inputs> = data => {
+    console.log(data);
+    reset();
+  };
+
   return (
     <Container className='container-login'>
       <Container className='container-login-wrapper'>
         <h2>Войти</h2>
-        <div className='form-group'>
-          <label htmlFor='username'>Имя пользователя</label>
-          <input
-            type='text'
-            id='username'
-            name='username'
-            placeholder='Введите имя пользователя'
-            required
-          />
-        </div>
-        <div className='form-group'>
-          <label htmlFor='email'>Email</label>
-          <input
-            type='email'
-            id='email'
-            name='email'
-            placeholder='Введите email'
-            required
-          />
-        </div>
-        <div className='form-group'>
-          <label htmlFor='password'>Пароль</label>
-          <input
-            type='password'
-            id='password'
-            name='password'
-            placeholder='Введите пароль'
-            required
-          />
-        </div>
-        <Button onClick={() => {}} title='Войти' />
+        <form
+          onSubmit={handleSubmit(onSubmit)}
+          autoComplete='off'
+          name='unique-form-name'>
+          <div className='form-group'>
+            <label htmlFor='email'>Email</label>
+            <input
+              type='text'
+              id='email'
+              placeholder='Введите email'
+              autoComplete='off'
+              {...register('email', {
+                required: 'Email is required',
+                pattern: {
+                  value: /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/,
+                  message: 'Incorrect email address',
+                },
+              })}
+            />
+          </div>
+          {errors.email && (
+            <span className='errorMessage'>{errors.email.message}</span>
+          )}
+          <div className='form-group'>
+            <label htmlFor='password'>Пароль</label>
+            <input
+              type='password'
+              id='password'
+              placeholder='Введите пароль'
+              autoComplete='off'
+              {...register('password', {
+                required: true,
+              })}
+            />
+          </div>
+          <div className='form-group'>
+            <label htmlFor='username'>Запомнить</label>
+            <input
+              type='checkbox'
+              id='rememberMe'
+              //   {...register('rememberMe', {
+              //     required: true,
+              //   })}
+            />
+          </div>
+          <Button onClick={() => {}} title='Войти' />
+        </form>
       </Container>
     </Container>
   );
