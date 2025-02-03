24b1af881a897790a7be59b56c56b17917cee1c7
EkaterinaZamkovoy <ekaterinazamkovoy@yandex.ru>
2025-02-03 18:05:42 +0300

    L20.2

--- src/index.tsx
+++ src/index.tsx
@@ -10,8 +10,7 @@ const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <BrowserRouter
-    basename={process.env.NODE_ENV === 'production' ? '/my-todolist' : '/'}>
+  <BrowserRouter>
     <Provider store={store}>
       <App />
     </Provider>
